3693ff072964570fa5f8b6bf50b6015ce9518275
silviaficara <silvia.ficara@kronos.tech>
2026-05-25 23:28:04 +0200

    feat(services): add test-servizio

--- lib/data/services.ts
+++ lib/data/services.ts
@@ -27,4 +27,10 @@ export const services: Service[] = [
       "Comunicazioni aziendali stabili e sicure, con gestione di rete, hosting e domini.",
     image: "/services/google-workspace.png",
   },
+  {
+    id: "test-servizio",
+    name: "test servizio",
+    description: "asldkfmlaksmdf",
+    image: "/services/test-servizio.png",
+  },
 ];
